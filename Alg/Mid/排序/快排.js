08155d89fbffcedd7531c7b56f24e08efcaf5f73
吕灿发 <lvcanfa@lvcanfadeMacBook-Air.local>
2023-08-01 23:53:42 +0800

    Promise的输出顺序，Promise.reslove().then中的return值对.then执行顺序的影响

--- Alg/Mid/排序/快排.js
+++ Alg/Mid/排序/快排.js
@@ -4,6 +4,11 @@ const arr = [5, 3, 8, 4, 2, 7, 1, 6];
 const sortedArr = myFastSort(arr);
 console.log(sortedArr); // 输出 [1, 2, 3, 4, 5, 6, 7, 8]
 
+/**
+ * @desc 普通快排
+ * @param {*} arr 
+ * @returns 
+ */
 function myFastSort(arr) {
     if (arr.length <= 1) {
         return arr
@@ -23,3 +28,51 @@ function myFastSort(arr) {
     }
     return [...myFastSort(left), baseItem, ...myFastSort(right)]
 }
+/**
+ * @desc 空间复杂度为 1 的快排
+ */
+
+function partition(arr, low, high) {
+    let pivot = arr[low]; // 选择第一个元素作为基准元素
+    let i = low + 1; // 左指针
+    let j = high; // 右指针
+    // let arr1 = [5, 2, 9, 8, 1, 7, 6];
+    while (true) {
+        while (i <= j && arr[i] < pivot) {
+            i++; // 左指针向右移动，直到找到大于等于基准元素的元素
+
+        }
+        while (i <= j && arr[j] > pivot) {
+            j--; // 右指针向左移动，直到找到小于等于基准元素的元素
+        }
+        if (i >= j) {
+            break; // 左指针和右指针相遇时退出循环
+        }
+        [arr[i], arr[j]] = [arr[j], arr[i]]; // 交换左指针和右指针所指向的元素
+        i++;
+        j--;
+    }
+    console.log(arr.join(''), i, j, low);
+    [arr[low], arr[j]] = [arr[j], arr[low]]; // 将基准元素放置到正确的位置上
+    console.log('+++++++',arr.join(''), i, j, low);
+
+    return j; // 返回基准元素的索引
+
+}
+
+function quicksort(arr, low, high) {
+    if (low < high) {
+        let pivotIndex = partition(arr, low, high); // 划分数组并获取基准元素的索引
+
+        quicksort(arr, low, pivotIndex - 1); // 对基准元素左侧的子数组进行递归排序
+
+        quicksort(arr, pivotIndex + 1, high); // 对基准元素右侧的子数组进行递归排序
+
+    }
+}
+// 示例用法
+
+let arr1 = [5, 2, 9, 8, 1, 7, 6];
+
+quicksort(arr1, 0, arr1.length - 1);
+console.log(arr1); // 输出 [1, 2, 5, 6, 7, 9]
---
+++ Dev/Promise/promise_output.js
@@ -0,0 +1,43 @@
+/**
+ * Promise输出
+ * .then中 return 的 其实是一个Promise.resolve()
+ * 1. 当我们return 一个常量的时候，执行完同步操作后就会注册下一个.then
+ * 2. 当 return 一个带有 then 的对象，则执行完同步之后不会立即执行下一个 .then ，
+ *    而是会注册 return 的对象中的 then，当其执行完后的下一步才会注册下一个 .then,相对于等待两次 then
+ * 3. 当return 的是一个 Promise.reslove()时，就执行完之后返回的是Promise.reslove(Promise.reslove()),
+ *    需要执行两次才会进入下一个.then 的注册  
+ */
+Promise.resolve()
+    .then((res) => {
+        console.log(0);
+        return 4
+        // return {
+        //     then: function (resolve) {
+        //         resolve(4)
+        //     }
+        // }
+        // return Promise.resolve(4)
+        return Promise.resolve(4)
+            .then(res=>{console.log('???',res)})
+            .then(()=>{console.log('!!!');})
+    })
+    .then((res)=>{
+        console.log(res,'res');
+    })
+    // 01
+Promise.resolve(-1)
+    .then((res)=>{
+        console.log(1);
+    })
+    .then((res)=>{
+        console.log(2);
+    })
+    .then((res)=>{
+        console.log(3);
+    })
+    .then(()=>{
+        console.log(5);
+    })
+    .then(()=>{
+        console.log(6);
+    })
